06ddc4a2504af5a552cb0fd3c8d4394220c77cde
shamil-ch <shamilkolekkara@gmail.com>
2024-07-20 11:06:11 +0530

    modified styles for thumbnail generation option

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -40,21 +40,21 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
 
       </div>
       {isAiThumbnail ? (
-        <div>
+        <div className="flex flex-col gap-5 mt-5">
           <div className='flex flex-col gap-2.5'>
             <Label className="text-16 font-bold text-white-1">
-              AI Prompt to generate podcast
+              AI Prompt to generate Thumbnail
             </Label>
             <Textarea 
             className='input-class font-light focus-visible:ring-offset-orange-1'
-            placeholder='Provide text to generate media'
+            placeholder='Provide text to generate thumbnail'
             rows={5}
             value={imagePrompt}
             onChange={(e) => setImagePrompt(e.target.value)}
             />
           </div>
 
-          <div className="mt-5 w-full max-w-[200px]">
+          <div className="w-full max-w-[200px]">
             <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" onClick={generateImage}>
               {isGenerating ? (
                 <>
