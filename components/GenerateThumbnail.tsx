47bb6a422eb176bf76834ecec30579d253393d71
shamil-ch <shamilkolekkara@gmail.com>
2024-07-23 11:09:34 +0530

    Generated custom thumbnail

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -7,15 +7,66 @@ import { GenerateThumbnailProps } from '@/types/indesx'
 import { Loader } from 'lucide-react'
 import { Input } from './ui/input'
 import Image from 'next/image'
+import { useToast } from './ui/use-toast'
+import { useMutation } from 'convex/react'
+import { useUploadFiles } from '@xixixao/uploadstuff/react'
+import { api } from '@/convex/_generated/api'
 
 const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt }: GenerateThumbnailProps) => {
 
   const [isAiThumbnail, setIsAiThumbnail] = useState(false)
   const [isImageLoading, setIsImageLoading] = useState(false)
-
   const imageRef = useRef<HTMLInputElement>(null);
+  const { toast } = useToast();
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { startUpload } = useUploadFiles(generateUploadUrl);
+  const getImageUrl = useMutation(api.podcasts.getUrl)
+
+
+  const handleImage = async (blob: Blob, fileName: string) => {
+    setIsImageLoading(true);
+    setImage('');
+
+    try {
+      const file = new File([blob], fileName, { type: 'image/png' });
+
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setImageStorageId(storageId)
+
+      const imageUrl = await getImageUrl({ storageId });
+      setImage(imageUrl!);
+      setIsImageLoading(false);
+      toast({
+        title: "Thumbnail generated successfully",
+      })
+    } catch (error) {
+      console.log(error);
+      toast({ title: 'Error generating thumbnail', variant: 'destructive'})
+    }
+  }
 
   const generateImage = async () => {}
+
+  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    try {
+      const files = e.target.files;
+      if(!files) return;
+      const file = files[0];
+      const blob = await file.arrayBuffer()
+      .then((ab) => new Blob([ab]));
+
+      handleImage(blob, file.name);
+    } catch (error) {
+      console.log(error);
+      toast({ title: 'Error uploading image', variant: 'destructive'})
+      
+    }
+  }
 
   return (
     <>
@@ -80,6 +131,7 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
               type="file"
               className='hidden'
               ref={imageRef}
+              onChange={(e) => uploadImage(e)}
             />
             {!isImageLoading ? (
               <Image 
@@ -94,17 +146,27 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, se
                 <Loader size={20} className='animate-spin'/>
               </div>
             )}
+            <div className='flex flex-col items-center gap-1 '>
+              <h2 className='text-12 font-bold text-orange-1'>
+                Click to upload
+              </h2>
+              <p className='text-12 font-normal text-gray-1'>
+                SVG, PNG, JPG, or GIF (max. 1080x1080px)
+              </p>
+            </div>
         </div>
       )}
-
-      <div className='flex flex-col items-center gap-1 '>
-        <h2 className='text-12 font-bold text-orange-1'>
-          Click to upload
-        </h2>
-        <p className='text-12 font-normal text-gray-1'>
-          SVG, PNG, JPG, or GIF (max. 1080x1080px)
-        </p>
-      </div>
+      {image && (
+        <div className='flex-center w-full'>
+          <Image 
+            src={image}
+            width={200}
+            height={200}
+            className='mt-5'
+            alt='thumbnail'
+          />
+        </div>
+      )}
     </>
   )
 }
